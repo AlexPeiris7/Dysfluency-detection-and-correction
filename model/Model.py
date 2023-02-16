b1895b99c725c69ed393c35a9ea19fbb4cd068c9
Alexio Peiris <61263760+AlexPeiris7@users.noreply.github.com>
2023-02-16 12:58:37 +0530

    Uploaded Model

--- model/Model.py
+++ model/Model.py
@@ -1,3 +1,9 @@
+# https://datascience.stackexchange.com/questions/45165/how-to-get-accuracy-f1-precision-and-recall-for-a-keras-model
+# https://keras.io/api/models/sequential/
+# https://www.tensorflow.org/api_docs/python/tf/keras/Sequential
+# https://www.tensorflow.org/api_docs/python/tf/keras/layers/Conv2D
+# https://stackoverflow.com/questions/51299836/what-values-are-returned-from-model-evaluate-in-keras
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from keras import layers
@@ -6,55 +12,48 @@ from keras import backend as K
 
 dataset = "/Users/alexiopeiris/Desktop/University/FourthYear/FYP/Imp/Stuttering_detection_and_correction/stuttering_detection/preprocessing/classified_spectrograms"
 
-IMAGE_COLORMODE = 'rgb'
-IMAGE_HEIGHT = 480
-IMAGE_WIDTH = 640
-BATCH_SIZE = 32
-N_CHANNELS = 3
-N_CLASSES = 2
-CLASS_WEIGHT = {0: 1, 1: 3.693}  # Balance dataset
-VALID_SPLIT = 0.1
-EPOCHS = 50
-
-train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+# splitting the dataset into 2 datasets one for testing the other for training by generating two tf datasets
+training_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     label_mode="categorical",
-    batch_size=BATCH_SIZE,
-    validation_split=VALID_SPLIT,
+    batch_size=32,
+    validation_split=0.1,
     directory=dataset,
     shuffle=True,
-    color_mode=IMAGE_COLORMODE,
-    image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
+    color_mode='rgb',
+    image_size=(480, 640),
     subset="training",
     seed=7,
 )
 
-valid_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+testing_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     label_mode="categorical",
-    batch_size=BATCH_SIZE,
-    validation_split=VALID_SPLIT,
+    batch_size=32,
+    validation_split=0.1,
     directory=dataset,
     shuffle=True,
-    color_mode=IMAGE_COLORMODE,
-    image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
+    color_mode='rgb',
+    image_size=(480, 640),
     subset="validation",
     seed=7,
 )
 
-
+# measuring how many positive predictions are correct
+# true positives (model predicting correctly the positive class)
 def precision(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
     pc = true_positives / (predicted_positives + K.epsilon())
     return pc
 
-
+# measuring how many positive classes are predicted correctly(positives only)
 def recall(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     rc = true_positives / (possible_positives + K.epsilon())
     return rc
 
-
+# measuring f1 score which combines recall and precision
+# 2* ((Precision * Recall)/(Precision + Recall))
 def f1(y_true, y_pred):
     pc = precision(y_true, y_pred)
     rc = recall(y_true, y_pred)
@@ -87,36 +86,30 @@ model = tf.keras.models.Sequential([
     layers.Dense(256, activation='relu'),
     layers.BatchNormalization(),
     layers.Dropout(0.5), # dropping data or noise intentionally to prevent the layer to adapt easily
-    layers.Dense(N_CLASSES, activation='softmax') # classifying image beased on output from convulutional layers
+    layers.Dense(2, activation='softmax') # classifying image beased on output from convulutional layers
 ])
-# input_shape = (None, 32, 32, 3)
-# model.build(input_shape)
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=['accuracy'])
 
-model.fit(train_dataset,
-          validation_data=valid_dataset,
-          epochs=10)
-model.summary()
-
-# Compile model
+# compile defines the loss function, optimizer and the metrics
+# these are defined as the model needs these to train
 model.compile(
     loss="categorical_crossentropy",
     optimizer='RMSprop',
     metrics=['accuracy', precision, recall, f1],
 )
 
-# Train model capture the history
+# Training model with given inputs and capturing the history
+# returns the history of accuracy, loss and others for every epoch
 history = model.fit(
-    train_dataset, epochs=EPOCHS,
-    validation_data=valid_dataset,
-    class_weight=CLASS_WEIGHT,
+    training_dataset, epochs=10,
+    validation_data=testing_dataset,
+    class_weight={0: 1, 1: 3.693},
 )
 
-# Compute the final loss, accuracy and etc.
+model.summary()
+
+# evaluating the loss, accuracy, precision, recall and f1 score
 final_loss, final_acc, final_precision, final_recall, f1_score = model.evaluate(
-    valid_dataset, verbose=0)
+    testing_dataset, verbose=0)
 
 print("Final loss: {0:.6f}, final accuracy: {1:.6f}, Final precision: {2:.6f}, Final recall: {3:.6f}, Final F1: {4:.6f}".format(
     final_loss,
@@ -138,8 +131,6 @@ def plot_metric(label, values, val_values):
     plt.legend()
     plt.savefig('/Users/alexiopeiris/Desktop/University/FourthYear/FYP/Imp/Stuttering_detection_and_correction/stuttering_detection/plots/' + label + '.png')
 
-
-# Plot the curves for training and validation.
 history_dict = history.history
 plot_metric('Loss', history_dict['loss'], history_dict['val_loss'])
 plot_metric('Accuracy', history_dict['accuracy'], history_dict['val_accuracy'])
